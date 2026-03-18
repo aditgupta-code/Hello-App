781d1d9a0aae7aa1e5ea3e81973647c083470d0a
Adit <aditgupta558@gmail.com>
2026-03-18 09:57:02 +0530

    feature/UC4-display-multiple-names

---
+++ HelloAppUC4.java
@@ -0,0 +1,20 @@
+/**
+ * HelloApp - UC4 - Display Hello with Multiple Command-Line Arguments or Default Message
+ * @author Adit
+ * @version 4.0
+ */
+public class HelloAppUC4 {
+    public static void main(String[] args) {
+        
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                String name = args[i];
+                System.out.println("Hello " + name);
+            }
+        } else {
+            String defaultName = "World";
+            System.out.println("Hello " + defaultName);
+        }
+        
+    }
+}
